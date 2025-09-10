8ebe4944c7337da9c9d9de68619383980e58762d
Ivysaur-Mrquestionmarks <75641314+Ivysaur-Mrquestionmarks@users.noreply.github.com>
2025-09-10 12:43:41 -0500

    spring

---
+++ 1ero/src/test/java/com/software/_ero/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software._ero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
